9b1e6a061e477a3a5bf9c9cbecc92444cf42c5c4
yindaheng98 <yindaheng98@gmail.com>
2025-05-24 17:01:40 -0700

    define attributes

---
+++ draco3d/draco3d/attributes.js
@@ -0,0 +1,7 @@
+// Attributes for the Draco 3D library
+const dracoAttributes = {
+    POSITION: { name: 'POSITION', type: Float32Array, stride: 3 },
+    NORMAL: { name: 'NORMAL', type: Float32Array, stride: 3 },
+    COLOR: { name: 'COLOR', type: Float32Array, stride: 3 },
+    TEX_COORD: { name: 'TEX_COORD', type: Float32Array, stride: 2 },
+};
