97ddb821af918f91f9ed7140647a6aa812b453a9
yindaheng98 <yindaheng98@gmail.com>
2025-05-24 21:48:05 -0700

    export attribute types

--- draco3d/index.js
+++ draco3d/index.js
@@ -1,6 +1,8 @@
+const attributes = require('./draco3d/attributes');
 const DracoEncoder = require('./encoder');
 const DracoDecoder = require('./decoder');
 module.exports = {
     DracoEncoder,
     DracoDecoder,
+    attributes,
 };
